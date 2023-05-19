d8babc4b03dde57b858694b0d56c41a1fb515e6d
Matthew Davis <matthewdavis1372@yahoo.com>
2023-05-19 01:41:28 -0400

    starting main homepage

--- my-react-website/src/App.js
+++ my-react-website/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
       <Navbar />
         <Routes>
-          <Route path='/' exact>
+          <Route exact path='/' element={<Home/>}>
 
           </Route>
         </Routes>
@@ -19,3 +20,33 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+// En el video vemos asi 
+//       <Router>
+//         <Navbar />
+//         <Switch>
+//           <Route path='/' exact component={Home} />
+//           <Route path='/services' component={Services} />
+//           <Route path='/products' component={Products} />
+//           <Route path='/sign-up' component={SignUp} />
+//         </Switch>
+//       </Router>
+// y ahora para que funcione (año 2022):
+//       <Router>
+//         <Navbar />
+//         <Routes>
+//           <Route exact path='/' element={<Home/>} />
+//           <Route path='/services' element={<Services/>} />
+//           <Route path='/products' element={<Products/>} />
+//           <Route path='/sign-up' element={<SignUp/>} />
+//         </Routes>
+//       </Router>
+// Saludos!
