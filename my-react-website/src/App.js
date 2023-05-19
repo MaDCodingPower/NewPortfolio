1356679d180467b307e3b88142bda651523ae833
Matthew Davis <matthewdavis1372@yahoo.com>
2023-05-18 20:26:26 -0400

    starting with routing, added react router dom

--- my-react-website/src/App.js
+++ my-react-website/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>yo</h1>
-    </div>
+    <>
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path='/' exact>
+
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
